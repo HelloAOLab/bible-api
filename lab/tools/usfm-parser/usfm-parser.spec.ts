f25e22d4013563912f9860b880978636e1ffc7f0
KallynGowdy <kallyngowdy@gmail.com>
2022-12-01 04:07:05 -0500

    Continue working on the parser

--- lab/tools/usfm-parser/usfm-parser.spec.ts
+++ lab/tools/usfm-parser/usfm-parser.spec.ts
@@ -1,6 +1,332 @@
-import { usfmParser } from './usfm-parser';
+import { isDigit,
+    isWhitespace,
+    loc,
+    marker,
+    t,
+    UsfmParser,
+    usfmParser,
+    UsfmTokenizer,
+    whitespace,
+    word
+} from './usfm-parser';
 
-describe('UsfmParser', () => {
+// describe('UsfmParser', () => {
     
+// })
+
+
+describe('UsfmTokenizer', () => {
+    let tokenizer: UsfmTokenizer;
+
+    beforeEach(() => {
+        tokenizer = new UsfmTokenizer();
+    });
+
+    describe('tokenize()', () => {
+        describe('markers', () => {
+            it('should be able to parse single character markers', () => {
+                const tokens = tokenizer.tokenize(`\\a`);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 2), 'marker'),
+                ]);
+            });
+
+            it('should be able to parse multiple character markers', () => {
+                const tokens = tokenizer.tokenize(`\\abc`);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 4), 'marker'),
+                ]);
+            });
+
+            it('should be able to parse markers with numbers', () => {
+                const tokens = tokenizer.tokenize(`\\abc123`);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 7), 'marker'),
+                ]);
+            });
+
+            it('should be able to parse end markers', () => {
+                const tokens = tokenizer.tokenize(`\\abc*`);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 5), 'marker'),
+                ]);
+            });
+
+            it('should be able to parse end markers with numbers', () => {
+                const tokens = tokenizer.tokenize(`\\abc123*`);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 8), 'marker'),
+                ]);
+            });
+
+            it('should not throw an error if the marker does not have a command', () => {
+                const tokens = tokenizer.tokenize(`\\`);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 1), 'marker')
+                ]);
+            });
+
+            it('should not throw an error if the marker has a number but not a command', () => {
+                const tokens = tokenizer.tokenize(`\\1`);
+                expect(tokens).toEqual([
+                    t(loc(0, 2), 'marker')
+                ]);
+            });
+
+            it('should be able to parse end markers that have text after them', () => {
+                const tokens = tokenizer.tokenize(`\\abc*z`);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 5), 'marker'),
+                    t(loc(5, 6), 'word'),
+                ]);
+            });
+
+            it('should be able to parse markers that have a space after them', () => {
+                const tokens = tokenizer.tokenize(`\\abc z`);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 4), 'marker'),
+                    t(loc(4, 5), 'whitespace'),
+                    t(loc(5, 6), 'word'),
+                ]);
+            });
+
+            it('should be able to parse markers that have a tab after them', () => {
+                const tokens = tokenizer.tokenize(`\\abc\tz`);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 4), 'marker'),
+                    t(loc(4, 5), 'whitespace'),
+                    t(loc(5, 6), 'word'),
+                ]);
+            });
+
+            it('should be able to parse markers that have a newline after them', () => {
+                const tokens = tokenizer.tokenize(`\\abc\nz`);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 4), 'marker'),
+                    t(loc(4, 5), 'whitespace'),
+                    t(loc(5, 6), 'word'),
+                ]);
+            });
+        });
+
+        describe('words', () => {
+            it('should be able to parse characters into words', () => {
+                const tokens = tokenizer.tokenize(`abc`);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 3), 'word'),
+                ]);
+            });
+        });
+
+        describe('whitespace', () => {
+            it('should be able to parse space into whitespace', () => {
+                const tokens = tokenizer.tokenize(` `);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 1), 'whitespace'),
+                ]);
+            });
+
+            it('should be able to parse newlines into whitespace', () => {
+                const tokens = tokenizer.tokenize(`\n`);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 1), 'whitespace'),
+                ]);
+            });
+            
+            it('should be able to parse carriage returns into whitespace', () => {
+                const tokens = tokenizer.tokenize(`\r`);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 1), 'whitespace'),
+                ]);
+            });
+
+            it('should be able to parse tabs into whitespace', () => {
+                const tokens = tokenizer.tokenize(`\t`);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 1), 'whitespace'),
+                ]);
+            });
+
+            it('should be able to parse multiple whitespace into whitespace', () => {
+                const tokens = tokenizer.tokenize(`\t \r\n`);
+
+                expect(tokens).toEqual([
+                    t(loc(0, 4), 'whitespace'),
+                ]);
+            });
+        });
+    });
 });
 
+describe('UsfmParser', () => {
+    let parser: UsfmParser;
+
+    beforeEach(() => {
+        parser = new UsfmParser();
+    });
+
+    describe('tokenize()', () => {
+        it('should be able to tokenize single character markers', () => {
+            const tokens = parser.tokenize(`\\a`);
+
+            expect(tokens).toEqual([
+                marker(loc(0, 2), '\\a', null, 'start')
+            ]);
+        });
+
+        it('should be able to tokenize multiple character markers', () => {
+            const tokens = parser.tokenize(`\\abc`);
+
+            expect(tokens).toEqual([
+                marker(loc(0, 4), '\\abc', null, 'start')
+            ]);
+        });
+
+        it('should be able to parse markers with numbers', () => {
+            const tokens = parser.tokenize(`\\abc123`);
+
+            expect(tokens).toEqual([
+                marker(loc(0, 7), '\\abc', 123, 'start'),
+            ]);
+        });
+
+        it('should be able to parse end markers', () => {
+            const tokens = parser.tokenize(`\\abc*`);
+
+            expect(tokens).toEqual([
+                marker(loc(0, 5), '\\abc', null, 'end'),
+            ]);
+        });
+
+        it('should be able to parse end markers with numbers', () => {
+            const tokens = parser.tokenize(`\\abc123*`);
+
+            expect(tokens).toEqual([
+                marker(loc(0, 8), '\\abc', 123, 'end'),
+            ]);
+        });
+
+        it('should throw an error if the marker does not have a command', () => {
+            expect(() => {
+                parser.tokenize(`\\`);
+            }).toThrowError();
+        });
+
+        it('should throw an error if the marker has a number but not a command', () => {
+            expect(() => {
+                parser.tokenize(`\\1`);
+            }).toThrowError();
+        });
+
+        it('should be able to parse end markers that have text after them', () => {
+            const tokens = parser.tokenize(`\\abc*z`);
+
+            expect(tokens).toEqual([
+                marker(loc(0, 5), '\\abc', null, 'end'),
+                word(loc(5, 6), 'z'),
+            ]);
+        });
+
+        it('should be able to parse markers that have a space after them', () => {
+            const tokens = parser.tokenize(`\\abc z`);
+
+            expect(tokens).toEqual([
+                marker(loc(0, 4), '\\abc'),
+                whitespace(loc(4, 5), ' '),
+                word(loc(5, 6), 'z'),
+            ]);
+        });
+
+        it('should be able to parse markers that have a tab after them', () => {
+            const tokens = parser.tokenize(`\\abc\tz`);
+
+            expect(tokens).toEqual([
+                marker(loc(0, 4), '\\abc'),
+                whitespace(loc(4, 5), '\t'),
+                word(loc(5, 6), 'z'),
+            ]);
+        });
+
+        it('should be able to parse markers that have a newline after them', () => {
+            const tokens = parser.tokenize(`\\abc\nz`);
+
+            expect(tokens).toEqual([
+                marker(loc(0, 4), '\\abc'),
+                whitespace(loc(4, 5), '\n'),
+                word(loc(5, 6), 'z'),
+            ]);
+        });
+    });
+
+});
+
+describe('isDigit()', () => {
+    it('should retrun true for all the ascii digits', () => {
+        for(let d = 0; d < 10; d++) {
+            let digit = d.toString();
+
+            expect(isDigit(digit)).toBe(true);
+        }
+    });
+
+    it('should return true for all digit code points in a string', () => {
+        let str = '0123456789';
+
+        for (let d of str) {
+            expect(isDigit(d)).toBe(true);
+        }
+    });
+
+    it('should return false for non digits', () => {
+        let str = 'abcdefghijkflmnopqrstuvwxyz';
+
+        for (let c of str) {
+            expect(isDigit(c)).toBe(false);
+        }
+    });
+
+    it('should return false for strings longer than length 1', () => {
+        let strings = [
+            '0a',
+            '0b',
+            '9a',
+            '9b',
+        ];
+
+        for (let str of strings) {
+            expect(isDigit(str)).toBe(false);
+        }
+    });
+});
+
+describe('isWhitespace()', () => {
+    it('should return true for common whitespace characters', () => {
+        let chars = [' ', '\n', '\t', '\r'];
+        for(let char of chars) {
+            expect(isWhitespace(char)).toBe(true);
+        }
+    });
+
+    it('should return false for non-whitespace characters', () => {
+        let str = 'abcdefghijkflmnopqrstuvwxyz0123456789';
+
+        for (let c of str) {
+            expect(isWhitespace(c)).toBe(false);
+        }
+    });
+});
--- lab/tools/usfm-parser/usfm-parser.ts
+++ lab/tools/usfm-parser/usfm-parser.ts
@@ -3,6 +3,268 @@ export function usfmParser() {
 }
 
 /**
+ * Defines a class that can tokenize a stream of characters into tokens.
+ */
+export class UsfmTokenizer {
+    private _input: string;
+    private _index: number;
+    private _start: number;
+
+    private get _tokenLength() {
+        return this._index - this._start;
+    }
+
+    /**
+     * Converts the given input into a list of tokens.
+     * @param input The input that should be tokenized.
+     */
+    tokenize(input: string): SimpleToken[] {
+        this._input = input;
+        this._index = 0;
+
+        return this._parseTokens();
+    }
+
+    private _parseTokens() {
+        let tokens: SimpleToken[] = [];
+        let token = this._parseToken();
+        while(token) {
+            tokens.push(token);
+            token = this._parseToken();
+        }
+
+        return tokens;
+    }
+
+    private _parseToken(): SimpleToken | null {
+        let state: 'none' | 'marker_start' | 'marker_number' | 'whitespace' | 'word' = 'none';
+        let kind: T | null = null;
+        this._start = this._index;
+        
+        while(this._index < this._input.length) {
+            const codePointNumber = this._input.codePointAt(this._index);
+
+            if (typeof codePointNumber === 'undefined') {
+                throw new Error('Unable to get code point!');
+            }
+
+            const codePoint = String.fromCodePoint(codePointNumber);
+
+            if (state === 'none') {
+                if (codePoint === '\\') {
+                    state = 'marker_start';
+                } else if (isWhitespace(codePoint)) {
+                    state = 'whitespace';
+                } else {
+                    state = 'word';
+                }
+            } else if (state === 'marker_start') {
+                if (isDigit(codePoint)) {
+                    if (this._tokenLength === 0) {
+                        throw new Error('Invalid Marker: Markers must not contain only digits.');
+                    }
+                    state = 'marker_number';
+                } else if (codePoint === '*') {
+                    this._index += codePoint.length;
+                    kind = 'marker';
+                    break;
+                } else if (isWhitespace(codePoint)) {
+                    kind = 'marker';
+                    break;
+                }
+            } else if(state === 'marker_number') {
+                if (codePoint === '*') {
+                    this._index += codePoint.length;
+                    kind = 'marker';
+                    break;
+                } else if (!isDigit(codePoint)) {
+                    kind = 'marker';
+                    break;
+                }
+            } else if (state === 'whitespace') {
+                if (!isWhitespace(codePoint)) {
+                    kind = 'whitespace';
+                    break;
+                }
+            } else if (state === 'word') {
+                if (isWhitespace(codePoint)) {
+                    kind = 'word';
+                    break;
+                }
+            }
+
+            this._index += codePoint.length;
+        }
+
+        if (!kind) {
+            if (this._index >= this._input.length) {
+                if (state == 'marker_start' || state === 'marker_number') {
+                    kind = 'marker';
+                } else if (state === 'word') {
+                    kind = 'word'
+                } else if (state === 'whitespace') {
+                    kind = 'whitespace';
+                }
+            }
+        }
+
+        if (kind) {
+            return t(
+                loc(this._start, this._index),
+                kind
+            );
+        }
+
+        return null;
+    }
+}
+
+export interface UsfmParseOptions {
+    paragraphs: Set<string>;
+}
+
+/**
+ * Defines a USFM Parser.
+ */
+export class UsfmParser {
+    
+    tokenize(input: string): Token[] {
+        const simpleTokens = new UsfmTokenizer().tokenize(input);
+        let tokens: Token[] = [];
+
+        for(let t of simpleTokens) {
+            if (t.kind === 'marker') {
+                let source = input.substring(t.loc.start, t.loc.end);
+                const isEnd = source.endsWith('*');
+
+                if (isEnd) {
+                    source = source.substring(0, source.length - 1);
+                }
+                
+                let numberIndex = -1;
+                for(let i = 0; i < source.length; i++) {
+                    if (isDigit(source[i])) {
+                        numberIndex = i;
+                        break;
+                    }
+                }
+
+                let number: number | null = null;
+                if (numberIndex === 1) {
+                    throw new Error('Markers must not be made only of numbers!');
+                }
+                if (numberIndex > 0) {
+                    number = parseInt(source.substring(numberIndex));
+                    source = source.substring(0, numberIndex);
+                }
+
+                if (source.length === 1) {
+                    throw new Error('Markers must have a command!');
+                }
+
+                tokens.push(
+                    marker(t.loc, source, number, isEnd ? 'end' : 'start')
+                );
+            } else if (t.kind === 'whitespace') {
+                let source = input.substring(t.loc.start, t.loc.end);
+                tokens.push(whitespace(t.loc, source));
+            } else if (t.kind === 'word') {
+                let source = input.substring(t.loc.start, t.loc.end);
+                tokens.push(word(t.loc, source));
+            }
+        }
+
+        return tokens;
+    }
+
+}
+
+/**
+ * Determines if the given character is a digit.
+ * @param char The character.
+ */
+export function isDigit(char: string): boolean {
+    return char.length === 1 && char >= '0' && char <= '9';
+}
+
+/**
+ * Determines if the given character is considered whitespace.
+ * @param char The character.
+ */
+export function isWhitespace(char: string): boolean {
+    return char === ' ' || char === '\t' || char === '\n' || char === '\r';
+}
+
+export function t(loc: SourceLocation, kind: T): SimpleToken {
+    return {
+        loc,
+        kind
+    };
+}
+
+/**
+ * Creates a new source location.
+ * @param start The start of the location.
+ * @param end The end of the location.
+ */
+export function loc(start: number, end: number): SourceLocation {
+    return {
+        start,
+        end
+    };
+}
+
+/**
+ * Creates a new marker token.
+ * @param loc The location for the token.
+ * @param command The command that the token contains.
+ * @param number The number that the marker contains.
+ * @param type The type of the marker.
+ */
+export function marker(loc: SourceLocation, command: string, number: number | null = null, type: MarkerToken['type'] = 'start'): MarkerToken {
+    return {
+        kind: 'marker',
+        loc,
+        command,
+        number,
+        type
+    };
+}
+
+/**
+ * Creates a new word token.
+ * @param loc The location for the token.
+ * @param word The word contained by the token.
+ */
+export function word(loc: SourceLocation, word: string): WordToken {
+    return {
+        kind: 'word',
+        loc,
+        word
+    };
+}
+
+export function whitespace(loc: SourceLocation, whitespace: string): WhitespaceToken {
+    return {
+        kind: 'whitespace',
+        loc,
+        whitespace
+    };
+}
+
+export type T = Token['kind'];
+
+/**
+ * Defines a simple token.
+ */
+export interface SimpleToken {
+    kind: T;
+    loc: SourceLocation;
+}
+
+export type Token = MarkerToken | WordToken | WhitespaceToken;
+
+/**
  * Defines an interface for a USFM marker node.
  * That is, the syntax for a marker.
  */
