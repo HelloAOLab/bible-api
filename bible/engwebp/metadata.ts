f949d44af67954ea4585003d374a4c1f5aecda48
KallynGowdy <kallyngowdy@gmail.com>
2023-01-14 00:26:02 -0500

    refactor: Rename webp to engwebp

---
+++ bible/engwebp/metadata.ts
@@ -0,0 +1,10 @@
+import { InputTranslationMetadata } from '../../src/usfm-parser/generator';
+
+export default {
+    id: 'ENGWEBP',
+    name: 'World English Bible',
+    shortName: 'WEBP',
+    language: 'en',
+    licenseUrl: 'https://ebible.org/find/show.php?id=engwebp',
+    website: 'https://worldenglish.bible/',
+} as InputTranslationMetadata;
