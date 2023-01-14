b7fe9f66f9871104b17fa2ed8b0d0cd9e1a030bb
KallynGowdy <kallyngowdy@gmail.com>
2023-01-14 02:23:34 -0500

    feat: Add the arbnav translation

---
+++ bible/arbnav/metadata.ts
@@ -0,0 +1,11 @@
+import { InputTranslationMetadata } from '../../src/usfm-parser/generator';
+
+export default {
+    id: 'ARBNAV',
+    name: 'كتاب الحياة',
+    englishName: 'New Arabic Version (Ketab El Hayat) Book of Life',
+    shortName: 'NAV',
+    language: 'arb',
+    licenseUrl: 'https://ebible.org/find/details.php?id=arbnav',
+    website: 'https://ebible.org/find/details.php?id=arbnav',
+} as InputTranslationMetadata;
