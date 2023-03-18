256a457172edc09d6c0fea92453feca2b4efd0e7
KallynGowdy <kallyngowdy@gmail.com>
2023-03-18 19:22:31 -0400

    feat: Add the ability to auto-detect the language direction

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,8 +9,24 @@ type Props = {
     language: string;
     children?: React.ReactNode
 };
+
+function getLanguageDirection(locale: string) {
+    let direction: string | null = null;
+    if (locale && Intl && Intl.Locale) {
+        const l: any = new Intl.Locale(locale);
+        const info = l.textInfo;
+
+        if (typeof info === 'object') {
+            direction = info.direction;
+        }
+    }
+
+    return direction;
+}
+
 const Layout = ({ language, children }: Props) => {
-    return <div className={`lang-${language}`}>
+    const dir = getLanguageDirection(language);
+    return <div className={`lang-${language}${dir ? ` dir-${dir}` : ''}`}>
         <LanguageProvider locale={language} defaultLocale="en">
             {children}
         </LanguageProvider>
