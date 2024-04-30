2a2130e37593e1e655c858629f71eb575a70fe48
KallynGowdy <kallyngowdy@gmail.com>
2024-04-30 15:07:52 -0400

    chore: Work on using a simple static site

--- src/pages/404.tsx
+++
@@ -1,9 +0,0 @@
-
-import * as React from 'react';
-import Layout from '../components/Layout';
-
-export default function _404() {
-    return <Layout>
-        <h1>Missing</h1>
-    </Layout>
-}
--- src/pages/test.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export default function () {
-    return <h1>Hello 123!</h1>
-}
-
-// export default 'abc';
