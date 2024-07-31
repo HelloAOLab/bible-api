d5159de05179d512c5101f20e730cda27818cc5e
KallynGowdy <kallyngowdy@gmail.com>
2024-07-31 17:51:38 -0400

    feat: Add the ability to clean generated files

---
+++ jakefile.js
@@ -0,0 +1,41 @@
+let { task, desc } = require('jake');
+const childProcess = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+function makeGlobbablePath(path) {
+    return path.replace(/\\/g, '/');
+}
+
+let folders = [
+    `${__dirname}/packages/helloao-tools`,
+    `${__dirname}/packages/helloao-cli`,
+];
+
+let patterns = [
+    `/**/*.js`,
+    `/**/*.js.map`,
+    `/**/*.ts.map`,
+    `/**/*.d.ts`,
+    `/*.tsbuildinfo`,
+];
+
+let negativePatterns = [`/typings/**/*`, `/node_modules/**/*`];
+
+let globs = [];
+folders.forEach((f) => {
+    patterns.forEach((p) => {
+        globs.push(f + p);
+    });
+
+    negativePatterns.forEach((p) => {
+        globs.push(`!${f}${p}`);
+    });
+});
+
+globs = globs.map((g) => makeGlobbablePath(g));
+
+task('clean', [], async function () {
+    const { deleteAsync } = await import('del');
+    const deleted = await deleteAsync(globs);
+});
