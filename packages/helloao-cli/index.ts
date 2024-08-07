469f353c25767cf4d76804178ea2caf303c2704e
KallynGowdy <kallyngowdy@gmail.com>
2024-08-07 16:06:07 -0400

    chore: Update some imports

--- packages/helloao-cli/index.ts
+++ packages/helloao-cli/index.ts
@@ -2,10 +2,14 @@ import * as db from './db';
 import * as downloads from './downloads';
 import * as uploads from './uploads';
 import * as actions from './actions';
+import * as files from './files';
+import * as s3 from './s3';
 
 export {
     db,
     downloads,
     uploads,
     actions,
+    files,
+    s3,
 };
--- packages/helloao-tools/parser/index.ts
+++ packages/helloao-tools/parser/index.ts
@@ -1,6 +1,7 @@
 export * from './types';
 export * from './usfm-parser';
 export * from './usx-parser';
+export * from './codex-parser';
 import * as iterators from './iterators';
 
 export { iterators };
