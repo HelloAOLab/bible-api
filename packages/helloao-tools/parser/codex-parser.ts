2798fb3b64019cf400b22820e8f4e66d64db02e0
keeandev <38674879+keeandev@users.noreply.github.com>
2024-12-12 17:43:28 -0600

    refactor: migrate parser to new HTML-based format

--- packages/helloao-tools/parser/codex-parser.ts
+++ packages/helloao-tools/parser/codex-parser.ts
@@ -15,15 +15,27 @@ export const chapterHeadingSchema = z.object({
     }),
 });
 
+export const verseSchema = z.object({
+    type: z.literal('text'),
+    id: z.string(),
+});
+
+export const paratextSchema = z.object({
+    type: z.literal('paratext'),
+    id: z.string(),
+});
+
 export const metadataSchema = z.discriminatedUnion('type', [
     chapterHeadingSchema,
+    verseSchema,
+    paratextSchema,
 ]);
 
 export const codexSchema = z.object({
     cells: z.array(
         z.object({
-            // kind: z.number().int(),
-            language: z.string(),
+            kind: z.number(),
+            languageId: z.string(),
             value: z.string(),
             metadata: z
                 .object({
@@ -49,9 +61,9 @@ export class CodexParser {
     private _noteCounter: number = 0;
 
     /**
-     * Parses the specified USX content.
+     * Parses the specified codex content.
      *
-     * @param usx The USX content to parse.
+     * @param codex The codex content to parse.
      * @returns The parse tree that was generated.
      */
     public parse(codex: string): ParseTree {
@@ -95,8 +107,86 @@ export class CodexParser {
             ]);
         }
 
+        function stripHTML(value: string) {
+            return value.replace(/<[^>]*>/g, '');
+        }
+
+        function toHeading(content: string) {
+            return {
+                type: 'heading',
+                content: [content],
+            } satisfies ChapterContent;
+        }
+
         for (let cell of data.cells) {
-            if (cell.language === 'scripture') {
+            if (cell.languageId === 'html') {
+                if (!cell.metadata) continue; // ignore html cells without metadata
+                const metadata = metadataSchema.parse(cell.metadata);
+                if (metadata.type === 'text') {
+                    const reference = parseVerseReference(metadata.id);
+                    if (!reference)
+                        throw new Error('Could not find verse reference.');
+
+                    if (!root.id) root.id = reference.book; // set book id if not already set
+
+                    const content = stripHTML(cell.value);
+
+                    // add line breaks in heading if there are multiple lines
+                    const lines = content
+                        .split('\n')
+                        .reduce((prev, current) => {
+                            if (prev.length === 0) return [current];
+                            else
+                                return [
+                                    ...prev,
+                                    {
+                                        lineBreak: true,
+                                    },
+                                    current,
+                                ] satisfies Verse['content'];
+                        }, [] as Verse['content']);
+
+                    addReference(reference, lines);
+                } else if (metadata.type === 'paratext') {
+                    const reference = parseVerseReference(`${metadata.id}:1`); // chapter headings do not have a verse reference always default to 1
+                    if (reference) {
+                        const content = stripHTML(cell.value);
+
+                        // add line breaks in heading if there are multiple lines
+                        const lines = content
+                            .split('\n')
+                            .reduce<ChapterContent[]>((prev, current) => {
+                                if (prev.length === 0)
+                                    return [toHeading(current)];
+                                else
+                                    return [
+                                        ...prev,
+                                        {
+                                            type: 'line_break',
+                                        },
+                                        toHeading(current),
+                                    ] satisfies ChapterContent[];
+                            }, []);
+
+                        lines.forEach((line) =>
+                            addChapterContent(reference.chapter, [line])
+                        );
+                    } else {
+                        // parse paratext that isn't a chapter reference as footnote
+                        const content = stripHTML(cell.value);
+                        if (!cell.metadata) {
+                            if (lastChapter) {
+                                (lastChapter as Chapter).footnotes.push({
+                                    noteId: this._noteCounter++,
+                                    text: content,
+                                    caller: null,
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+            /* if (cell.language === 'scripture') {
                 const lines = cell.value.split('\n');
                 const references = lines.map(
                     (l) => parseVerseReference(l) ?? l
@@ -160,29 +250,7 @@ export class CodexParser {
                         addReference(ref, content);
                     }
                 }
-            } else if (cell.language === 'markdown') {
-                if (cell.metadata) {
-                    if (cell.metadata.type === 'chapter-heading') {
-                        const metadata = metadataSchema.parse(cell.metadata);
-
-                        const chapter = metadata.data.chapter;
-                        addChapterContent(chapter, [
-                            {
-                                type: 'heading',
-                                content: [cell.value],
-                            },
-                        ]);
-                    }
-                } else {
-                    if (lastChapter) {
-                        (lastChapter as Chapter).footnotes.push({
-                            noteId: this._noteCounter++,
-                            text: cell.value,
-                            caller: null,
-                        });
-                    }
-                }
-            }
+            } */
         }
 
         for (let chapter of chapters.values()) {
