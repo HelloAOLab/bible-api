d47e87c722094c36f747319d7a54e78266b094b1
Kallyn Gowdy <kallyngowdy@gmail.com>
2026-05-04 12:06:58 -0400

    test: Update tests for Audio URLs

--- packages/helloao-tools/generation/audio.spec.ts
+++ packages/helloao-tools/generation/audio.spec.ts
@@ -17,50 +17,27 @@ describe('capitalize()', () => {
 
 describe('KNOWN_AUDIO_TRANSLATIONS', () => {
     const cases = [
-        // Gilbert
-        [
-            'BSB',
-            'gilbert',
-            'GEN',
-            1,
-            'https://openbible.com/audio/gilbert/BSB_01_Gen_001_G.mp3',
-        ] as const,
-        [
-            'BSB',
-            'gilbert',
-            '1SA',
-            5,
-            'https://openbible.com/audio/gilbert/BSB_09_1Sa_005_G.mp3',
-        ] as const,
-        [
-            'BSB',
-            'gilbert',
-            'PSA',
-            110,
-            'https://openbible.com/audio/gilbert/BSB_19_Psa_110_G.mp3',
-        ] as const,
-
         // Hays
         [
             'BSB',
             'hays',
             'GEN',
             1,
-            'https://openbible.com/audio/hays/BSB_01_Gen_001_H.mp3',
+            'https://audio.bible.helloao.org/api/BSB/GEN/1/audio/hays.mp3',
         ] as const,
         [
             'BSB',
             'hays',
             '1SA',
             5,
-            'https://openbible.com/audio/hays/BSB_09_1Sa_005_H.mp3',
+            'https://audio.bible.helloao.org/api/BSB/1SA/5/audio/hays.mp3',
         ] as const,
         [
             'BSB',
             'hays',
             'PSA',
             110,
-            'https://openbible.com/audio/hays/BSB_19_Psa_110_H.mp3',
+            'https://audio.bible.helloao.org/api/BSB/PSA/110/audio/hays.mp3',
         ] as const,
 
         // Souer
@@ -69,21 +46,44 @@ describe('KNOWN_AUDIO_TRANSLATIONS', () => {
             'souer',
             'GEN',
             1,
-            'https://openbible.com/audio/souer/BSB_01_Gen_001.mp3',
+            'https://audio.bible.helloao.org/api/BSB/GEN/1/audio/souer.mp3',
         ] as const,
         [
             'BSB',
             'souer',
             '1SA',
             5,
-            'https://openbible.com/audio/souer/BSB_09_1Sa_005.mp3',
+            'https://audio.bible.helloao.org/api/BSB/1SA/5/audio/souer.mp3',
         ] as const,
         [
             'BSB',
             'souer',
             'PSA',
             110,
-            'https://openbible.com/audio/souer/BSB_19_Psa_110.mp3',
+            'https://audio.bible.helloao.org/api/BSB/PSA/110/audio/souer.mp3',
+        ] as const,
+
+        // David
+        [
+            'BSB',
+            'david',
+            'GEN',
+            1,
+            'https://audio.bible.helloao.org/api/BSB/GEN/1/audio/david.mp3',
+        ] as const,
+        [
+            'BSB',
+            'david',
+            '1SA',
+            5,
+            'https://audio.bible.helloao.org/api/BSB/1SA/5/audio/david.mp3',
+        ] as const,
+        [
+            'BSB',
+            'david',
+            'PSA',
+            110,
+            'https://audio.bible.helloao.org/api/BSB/PSA/110/audio/david.mp3',
         ] as const,
     ];
 
