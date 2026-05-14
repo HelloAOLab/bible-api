642db827a6face8f722f44f4e4861a7334217b35
Kallyn Gowdy <kallyngowdy@gmail.com>
2026-05-14 12:09:25 -0400

    refactor: Use the new reference properties for next/previous chapters and first/last chapters

--- packages/free-use-bible-api/apis/FreeUseBibleApi.ts
+++ packages/free-use-bible-api/apis/FreeUseBibleApi.ts
@@ -1,26 +1,84 @@
 import * as runtime from '../runtime';
-import { ApiTranslationBookChapter, ApiTranslationBookChapterFromJSON } from "../models";
-import { DefaultApi } from "./DefaultApi";
+import {
+    ApiTranslationBook,
+    ApiTranslationBookChapter,
+    ApiTranslationBookChapterFromJSON,
+} from '../models';
+import { DefaultApi } from './DefaultApi';
 
 export class FreeUseBibleApi extends DefaultApi {
     /**
-     * Gets the chapter that follows the given chapter, or null if there is no next chapter.
-     * @param chapter The chapter to get the next chapter of.
+     * Gets the first chapter of the given book.
+     * @param book The book to get the first chapter of.
      * @param initOverrides Optional overrides for the request.
-     * @returns The next chapter, or null if there is no next chapter.
+     * @returns The first chapter of the given book.
      */
-    async getNextChapterRaw(chapter: ApiTranslationBookChapter, initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<runtime.ApiResponse<ApiTranslationBookChapter> | null> {
-        if (!chapter.nextChapterApiLink) {
-            return null;
-        }
+    async getTranslationBookFirstChapterRaw(
+        book: ApiTranslationBook,
+        initOverrides?: RequestInit | runtime.InitOverrideFunction
+    ): Promise<runtime.ApiResponse<ApiTranslationBookChapter>> {
+        return this.getTranslationBookChapterRaw(
+            {
+                translation: book.firstChapterReference.translationId,
+                book: book.firstChapterReference.book,
+                chapter: book.firstChapterReference.chapter,
+            },
+            initOverrides
+        );
+    }
 
-        const response = await this.request({
-            path: chapter.nextChapterApiLink,
-            method: 'GET',
-            headers: {},
-        }, initOverrides);
+    /**
+     * Gets the first chapter of the given book.
+     * @param book The book to get the first chapter of.
+     * @param initOverrides Optional overrides for the request.
+     * @returns The first chapter of the given book.
+     */
+    async getTranslationBookFirstChapter(
+        book: ApiTranslationBook,
+        initOverrides?: RequestInit | runtime.InitOverrideFunction
+    ): Promise<ApiTranslationBookChapter> {
+        const apiResponse = await this.getTranslationBookFirstChapterRaw(
+            book,
+            initOverrides
+        );
+        return await apiResponse.value();
+    }
 
-        return new runtime.JSONApiResponse(response, (jsonValue) => ApiTranslationBookChapterFromJSON(jsonValue));
+    /**
+     * Gets the last chapter of the given book.
+     * @param book The book to get the last chapter of.
+     * @param initOverrides Optional overrides for the request.
+     * @returns The last chapter of the given book.
+     */
+    async getTranslationBookLastChapterRaw(
+        book: ApiTranslationBook,
+        initOverrides?: RequestInit | runtime.InitOverrideFunction
+    ): Promise<runtime.ApiResponse<ApiTranslationBookChapter>> {
+        return this.getTranslationBookChapterRaw(
+            {
+                translation: book.lastChapterReference.translationId,
+                book: book.lastChapterReference.book,
+                chapter: book.lastChapterReference.chapter,
+            },
+            initOverrides
+        );
+    }
+
+    /**
+     * Gets the last chapter of the given book.
+     * @param book The book to get the last chapter of.
+     * @param initOverrides Optional overrides for the request.
+     * @returns The last chapter of the given book.
+     */
+    async getTranslationBookLastChapter(
+        book: ApiTranslationBook,
+        initOverrides?: RequestInit | runtime.InitOverrideFunction
+    ): Promise<ApiTranslationBookChapter> {
+        const apiResponse = await this.getTranslationBookLastChapterRaw(
+            book,
+            initOverrides
+        );
+        return await apiResponse.value();
     }
 
     /**
@@ -29,8 +87,38 @@ export class FreeUseBibleApi extends DefaultApi {
      * @param initOverrides Optional overrides for the request.
      * @returns The next chapter, or null if there is no next chapter.
      */
-    async getNextChapter(chapter: ApiTranslationBookChapter, initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<ApiTranslationBookChapter | null> {
-        const apiResponse = await this.getNextChapterRaw(chapter, initOverrides);
+    async getTranslationNextChapterRaw(
+        chapter: ApiTranslationBookChapter,
+        initOverrides?: RequestInit | runtime.InitOverrideFunction
+    ): Promise<runtime.ApiResponse<ApiTranslationBookChapter> | null> {
+        if (!chapter.nextChapterReference) {
+            return null;
+        }
+
+        return this.getTranslationBookChapterRaw(
+            {
+                translation: chapter.nextChapterReference.translationId,
+                book: chapter.nextChapterReference.book,
+                chapter: chapter.nextChapterReference.chapter,
+            },
+            initOverrides
+        );
+    }
+
+    /**
+     * Gets the chapter that follows the given chapter, or null if there is no next chapter.
+     * @param chapter The chapter to get the next chapter of.
+     * @param initOverrides Optional overrides for the request.
+     * @returns The next chapter, or null if there is no next chapter.
+     */
+    async getTranslationNextChapter(
+        chapter: ApiTranslationBookChapter,
+        initOverrides?: RequestInit | runtime.InitOverrideFunction
+    ): Promise<ApiTranslationBookChapter | null> {
+        const apiResponse = await this.getTranslationNextChapterRaw(
+            chapter,
+            initOverrides
+        );
         return apiResponse ? await apiResponse.value() : null;
     }
 
@@ -40,17 +128,22 @@ export class FreeUseBibleApi extends DefaultApi {
      * @param initOverrides Optional overrides for the request.
      * @returns The previous chapter, or null if there is no previous chapter.
      */
-    async getPreviousChapterRaw(chapter: ApiTranslationBookChapter, initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<runtime.ApiResponse<ApiTranslationBookChapter> | null> {
-        if (!chapter.previousChapterApiLink) {
+    async getTranslationPreviousChapterRaw(
+        chapter: ApiTranslationBookChapter,
+        initOverrides?: RequestInit | runtime.InitOverrideFunction
+    ): Promise<runtime.ApiResponse<ApiTranslationBookChapter> | null> {
+        if (!chapter.previousChapterReference) {
             return null;
         }
-        const response = await this.request({
-            path: chapter.previousChapterApiLink,
-            method: 'GET',
-            headers: {},
-        }, initOverrides);
 
-        return new runtime.JSONApiResponse(response, (jsonValue) => ApiTranslationBookChapterFromJSON(jsonValue));
+        return this.getTranslationBookChapterRaw(
+            {
+                translation: chapter.previousChapterReference.translationId,
+                book: chapter.previousChapterReference.book,
+                chapter: chapter.previousChapterReference.chapter,
+            },
+            initOverrides
+        );
     }
 
     /**
@@ -59,8 +152,14 @@ export class FreeUseBibleApi extends DefaultApi {
      * @param initOverrides Optional overrides for the request.
      * @returns The previous chapter, or null if there is no previous chapter.
      */
-    async getPreviousChapter(chapter: ApiTranslationBookChapter, initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<ApiTranslationBookChapter | null> {
-        const apiResponse = await this.getPreviousChapterRaw(chapter, initOverrides);
+    async getTranslationPreviousChapter(
+        chapter: ApiTranslationBookChapter,
+        initOverrides?: RequestInit | runtime.InitOverrideFunction
+    ): Promise<ApiTranslationBookChapter | null> {
+        const apiResponse = await this.getTranslationPreviousChapterRaw(
+            chapter,
+            initOverrides
+        );
         return apiResponse ? await apiResponse.value() : null;
     }
 }
